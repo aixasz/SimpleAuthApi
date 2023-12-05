d51bc9a1ca4ff87743ca425f633a860ab2b517a0
Nattapong Nunpan <aixasz@hotmail.com>
2023-12-05 18:08:33 +0700

    Update User Management Service

---
+++ src/SimpleAuthApi/Services/IUserManagementService.cs
@@ -0,0 +1,13 @@
+﻿using SimpleAuthApi.Domain.Models;
+
+namespace SimpleAuthApi.Services;
+
+public interface IUserManagementService
+{
+    Task<UserViewModel?> GetAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<UserViewModel[]> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<UserViewModel[]> SearchAsync(string term, CancellationToken cancellationToken = default);
+    Task<UserViewModel> CreateAsync(UserCreateModel userCreateModel, CancellationToken cancellationToken = default);
+    Task<UserViewModel> UpdateAsync(UserViewModel userUpdateModel, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid userId, CancellationToken cancellationToken = default);
+}
--- src/SimpleAuthApi/Services/UserManagementService.cs
+++ src/SimpleAuthApi/Services/UserManagementService.cs
@@ -1,54 +1,101 @@
-﻿using SimpleAuthApi.Database;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SimpleAuthApi.Domain;
+using SimpleAuthApi.Domain.Entities;
+using SimpleAuthApi.Domain.Models;
 
 namespace SimpleAuthApi.Services;
 
-public interface IUserManagementService
+public class UserManagementService(
+    AppDbContext dbContext,
+    UserManager<User> userManager,
+    IUserStore<User> userStore,
+    IUserEmailStore<User> emailStore) : IUserManagementService
 {
-    Task<User> GetAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<IReadOnlyList<User>> GetAllAsync(CancellationToken cancellationToken = default);
-    Task<IReadOnlyList<User>> SearchAsync(string term, CancellationToken cancellationToken = default);
-    Task<User> CreateAsync(User user, CancellationToken cancellationToken = default);
-    Task<User> UpdateAsync(User user, CancellationToken cancellationToken = default);
-    Task DeleteAsync(User user, CancellationToken cancellationToken = default);
-}
+    private readonly AppDbContext dbContext = dbContext;
+    private readonly IUserStore<User> userStore;
+    private readonly IUserEmailStore<User> emailStore;
+    private readonly UserManager<User> userManager = userManager;
 
-public class UserManagementService : IUserManagementService
-{ 
-    private readonly AppDbContext dbContext;
-
-    public UserManagementService(AppDbContext dbContext)
+    public async Task<UserViewModel[]> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        this.dbContext = dbContext;
+        var users = await dbContext.Users.ToListAsync(cancellationToken);
+
+        return users.Select(FromEntity).ToArray();
     }
 
-    public Task<IReadOnlyList<User>> GetAllAsync(CancellationToken cancellationToken = default)
+    public async Task<UserViewModel?> GetAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var user = await dbContext.Users.SingleOrDefaultAsync(user => user.Id == id, cancellationToken);
+        return user is not null ? FromEntity(user) : null;
     }
 
-    public Task<User> GetAsync(Guid id, CancellationToken cancellationToken = default)
+    public async Task<UserViewModel[]> SearchAsync(string term, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var users = await dbContext.Users.Where(user =>
+            user.FirstName.Contains(term) || user.LastName.Contains(term)
+        ).ToArrayAsync(cancellationToken);
+
+        return users.Select(FromEntity).ToArray();
     }
 
-    public Task<IReadOnlyList<User>> SearchAsync(string term, CancellationToken cancellationToken = default)
+    public async Task<UserViewModel> CreateAsync(UserCreateModel createModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var user = new User
+        {
+            FirstName = createModel.FirstName,
+            LastName = createModel.LastName,
+        };
+
+        await userStore.SetUserNameAsync(user, createModel.Email, cancellationToken);
+        await emailStore.SetEmailAsync(user, createModel.Email, cancellationToken);
+
+        var result = await userManager.CreateAsync(user, createModel.Password);
+
+        if (!result.Succeeded)
+        {
+            var message = string.Join(
+                Environment.NewLine,
+                result.Errors.Select(error => $"{error.Code} : {error.Description}")
+            );
+            throw new Exception(message);
+        }
+
+        return FromEntity(user);
     }
 
-    public Task<User> CreateAsync(User user, CancellationToken cancellationToken = default)
+    public async Task DeleteAsync(Guid userId, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var user = await dbContext.Users.FirstOrDefaultAsync(user => user.Id == userId, cancellationToken);
+        if (user is not null)
+        {
+            dbContext.Remove(user);
+            await dbContext.SaveChangesAsync(cancellationToken);
+        }
     }
 
-    public Task DeleteAsync(User user, CancellationToken cancellationToken = default)
+    public async Task<UserViewModel> UpdateAsync(UserViewModel userUpdateModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var user = await dbContext.Users.SingleOrDefaultAsync(user => user.Id == userUpdateModel.Id, cancellationToken);
+        if (user is not null)
+        {
+            user.FirstName = userUpdateModel.FirstName;
+            user.LastName = userUpdateModel.LastName;
+
+            dbContext.Users.Update(user);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return FromEntity(user);
+        }
+
+        return userUpdateModel;
     }
 
-
-    public Task<User> UpdateAsync(User user, CancellationToken cancellationToken = default)
+    private static UserViewModel FromEntity(User user) => new()
     {
-        throw new NotImplementedException();
-    }
+        Email = user.Email!,
+        FirstName = user.FirstName,
+        LastName = user.LastName,
+        Id = user.Id,
+    };
 }
